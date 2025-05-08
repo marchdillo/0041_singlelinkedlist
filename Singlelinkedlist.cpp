eea3c8585358fcd25bbc49bc201a1d8d3dd51070
marchdillo <kevinno.limiy@gmail.com>
2025-05-08 20:49:47 +0700

    Commit 8

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -87,4 +87,20 @@ public:
         delete current;
         return true;
     }
-}
+    void traverse()
+    {
+        if (listEmpty())
+            cout << "\nList Kosong\n";
+        else
+        {
+            cout << "\nData dalam list adalah:\n";
+            Node *currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+    }
+};
