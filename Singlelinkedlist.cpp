cda204ec0eb8915ae9bcb9517b97e4c885b9257e
marchdillo <kevinno.limiy@gmail.com>
2025-05-08 20:53:21 +0700

    Commit 10

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -110,4 +110,77 @@ int main()
     LinkedList mhs;
     int nim;
     char ch;
+    do
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Menambah data ke dalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data dalam List" << endl;
+        cout << "4. Mencari data dalam List" << endl;
+        cout << "5. Keluar\n"
+             << endl;
+
+        cout << "Masukkan pilihan (1-5): ";
+        cin >> ch;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            mhs.addNode();
+            break;
+        }
+        case '2':
+        {
+            if (mhs.listEmpty())
+            {
+                cout << "\nList Kosong" << endl;
+                break;
+            }
+            cout << "\nMasukkan nim mahasiswa yang akan dihapus : ";
+            cin >> nim;
+
+            if (mhs.delNode(nim) == false)
+                cout << "Data tidak ditemukan" << endl;
+            else
+                cout << "Data dengan nomor mahasiswa " << nim << " berhasil dihapus " << endl;
+
+            break;
+        }
+        case '3':
+        {
+            mhs.traverse();
+            break;
+        }
+        case '4':
+        {
+            if (mhs.listEmpty() == true)
+            {
+                cout << "\nList Kosong\n";
+                break;
+            }
+            Node *previous, *current;
+            cout << "\nMasukkan nomor mahasiswa yang dicari : ";
+            cin >> nim;
+            if (mhs.Search(nim, previous, current) == false)
+                cout << "\nData tidak ditemukan" << endl;
+            else
+            {
+                cout << "\nData ditemukan" << endl;
+                cout << "\nNIM Mahasiswa: " << current->noMhs << endl;
+                cout << endl;
+            }
+            break;
+        }
+        case '5':
+        {
+            break;
+        }
+        default:
+        {
+            cout << "Pilihan salah!." << endl;
+            break;
+        }
+        }
+    } while (ch != '5');
 }
