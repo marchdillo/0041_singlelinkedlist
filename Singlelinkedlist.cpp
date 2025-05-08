73f0f7cecf2058fda3d7c848602b57b41b754678
marchdillo <kevinno.limiy@gmail.com>
2025-05-08 20:48:01 +0700

    Commit 7

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -73,4 +73,18 @@ public:
         }
         return (current != NULL);
     }
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+        if (Search(nim, previous, current) == false)
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
+    }
 }
